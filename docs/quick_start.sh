75a2bffc865dde486bb5b78277f06561784333a7
Eric Wong <eric.t.wong@cgi.com>
2017-05-01 15:11:09 -0400

    Adding event tables.

--- docs/quick_start.sh
+++ docs/quick_start.sh
@@ -5,11 +5,13 @@ cat ../db/tables/EMPLOYEE.sql >> temp.sql
 cat ../db/tables/POINT_TALLY.sql >> temp.sql
 cat ../db/tables/REF_POINTS.sql >> temp.sql
 cat ../db/tables/LOAD_AUD.sql >> temp.sql
+cat ../db/tables/EVENT.sql >> temp.sql
 cat ../db/mock_data/ACTIVITY.sql >> temp.sql
 cat ../db/mock_data/EMPLOYEE.sql >> temp.sql
 cat ../db/mock_data/POINT_TALLY.sql >> temp.sql
 cat ../db/mock_data/REF_POINTS.sql >> temp.sql
 cat ../db/mock_data/LOAD_AUD.sql >> temp.sql
+cat ../db/mock_data/EVENT.sql >> temp.sql
 
 mysql -u root -p < temp.sql
 rm temp.sql
