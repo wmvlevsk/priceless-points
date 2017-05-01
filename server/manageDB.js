5ffd9b8644ff20d5324886ee393c4d362163728f
Eric Wong <eric.t.wong@cgi.com>
2017-05-01 11:21:47 -0400

    Added protection against SQL injection, restructured files for services, added calculated column for ref_points, added employee API calls.

--- server/manageDB.js
+++ server/manageDB.js
@@ -1,5 +1,24 @@
 const pool = require('./mysqlConnector');
 
+exports.executeQueryWithParams=function(query, param, callback){
+    pool.getConnection(function(err,connection){
+        if (err) {
+          connection.release();
+          throw err;
+        }   
+        connection.query(query, param, function(err,rows){
+            connection.release();
+            if(!err) {
+                callback(null, {rows: rows});
+            }           
+        });
+        connection.on('error', function(err) {      
+              throw err;
+              return;     
+        });
+    });
+}
+
 exports.executeQuery=function(query,callback){
     pool.getConnection(function(err,connection){
         if (err) {
--- server/routes/api.js
+++ server/routes/api.js
@@ -24,10 +24,29 @@ router.get('/posts', (req, res) => {
     });
 });
 
-// Get all posts
-router.get('/employees', (req, res) => {
 
-  manageDB.executeQuery('SELECT * FROM employee', function (err, data) {
+/**
+ * Employee information from text
+ * Also has escaped parameters to protect against SQL injection.
+ * @param {string} /:q
+ */
+router.get('/employees/:q', (req, res) => {
+  var query_string = "%".concat(req.params.q, "%");
+  var query_num = req.params.q;
+  manageDB.executeQueryWithParams('SELECT * FROM EMPLOYEE WHERE FIRST_NAME LIKE ? OR LAST_NAME LIKE ? OR EMPLOYEE_ID = ?', [query_string, query_string, query_num], function (err, data) {
+    res.status(200).json(JSON.stringify(data.rows));
+  });
+});
+
+
+/**
+ * Employee information from ID
+ * Also has escaped parameters to protect against SQL injection.
+ * @param {int} /:id
+ */
+router.get('/employee/:id', (req, res) => {
+  var e_id = req.params.id;
+  manageDB.executeQueryWithParams('SELECT e.first_name, e.last_name, a.activity_name, a.point_value FROM EMPLOYEE e LEFT JOIN POINT_TALLY pt ON e.employee_id = pt.employee_id INNER JOIN ACTIVITY a ON pt.activity_id = a.id WHERE e.employee_id = ?', [e_id], function (err, data) {
     res.status(200).json(JSON.stringify(data.rows));
   });
 });
