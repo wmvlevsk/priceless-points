86003f8de6c07ac1707c9889c436b818e7944c66
Eric Wong <eric.t.wong@cgi.com>
2017-05-03 15:19:18 -0400

    Updating with quarter information

--- src/app/leaderboard/leaderboard.component.ts
+++ src/app/leaderboard/leaderboard.component.ts
@@ -9,6 +9,7 @@ import { LeaderboardService } from './leaderboard.service';
 export class LeaderboardComponent implements OnInit {
   // instantiate posts to an empty array
   points: any = [];
+  dateInformation: Object;
 
   constructor(private leaderboardService: LeaderboardService) { }
 
@@ -19,6 +20,22 @@ export class LeaderboardComponent implements OnInit {
     this.leaderboardService.getFullList().subscribe(points => {
       this.points = points;
     });
+    this.dateInformation = this.daysLeftInQuarter();
+  }
+
+  daysLeftInQuarter() {
+    var today = new Date();
+    var quarter = Math.floor((today.getMonth() + 3) / 3);
+    var nextq;
+    if (quarter == 4) {
+      nextq = new Date(today.getFullYear() + 1, 1, 1);
+    } else {
+      nextq = new Date(today.getFullYear(), quarter * 3, 1);
+    }
+    var millis1 = today.getTime();
+    var millis2 = nextq.getTime();
+    return {daysLeft: Math.round((millis2 - millis1) / 1000 / 60 / 60 / 24),
+    currentQuarter: quarter};
   }
 
 }
